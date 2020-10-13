a01812a29404f500ffccf26759e74942d198f50e
Devin Cooper <dcoope12@asu.edu>
2020-10-13 02:21:36 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -19,8 +19,7 @@ class Person
   end
 
   class PartnerError < StandardError
-    #Raise the PartnerError when the argument passed into get_married
-    #is not an instance of the Person class
+
     def message
       "you must give the get_married method an argument of
       an instance of the person class!"
